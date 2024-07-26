4e678c85637b15921d169e7173e304a96d42706c
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-07-26 12:47:18 -0500

    HU6 Create Employee and security of dish and restaurant

---
+++ plazoleta_microservice/src/main/java/com/pragma/plazoleta_microservice/adapters/driven/jpa/mysql/repository/IEmployeeWithRestaurantRepository.java
@@ -0,0 +1,17 @@
+package com.pragma.plazoleta_microservice.adapters.driven.jpa.mysql.repository;
+
+import com.pragma.plazoleta_microservice.adapters.driven.jpa.mysql.entity.EmployeeWithRestaurantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface IEmployeeWithRestaurantRepository extends JpaRepository<EmployeeWithRestaurantEntity, Long> {
+
+    @Query("SELECT e FROM EmployeeWithRestaurantEntity e WHERE e.userId = :userId")
+    Optional< EmployeeWithRestaurantEntity> findRestaurantIdByEmployee(Long userId);
+
+
+
+
+}
